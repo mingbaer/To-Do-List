faae57cc9cee6de0c7e27f5603bc2275f3b8a1a9
Alexandra Baer Chan <mingbaer@gmail.com>
2024-05-05 15:42:56 -0500

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Alexandra Baer Chan on 5/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Alexandra Baer Chan on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
